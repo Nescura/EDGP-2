30683476134c43005b65cd3d19898618adce0584
Tamanegi-K <43199511+Tamanegi-K@users.noreply.github.com>
2023-03-01 19:46:34 +0800

    Fixing Merge Conflicts
    - basically bringing back my now discarded virus timer fill design, replacing it with mingrui's and putting it nicely in the canvas

---
+++ Popup Defender/Assets/2. Scripts/LayerAppending.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerAppending : MonoBehaviour
+{
+	[Header("Helps to define layering order in their own panels")]
+	[Header("MAKE SURE ALL MINIGAME OBJECRS WITH SPRITE RENDERERS HAVE THIS SCRIPT!")]
+	public int indexToAppend;
+}
