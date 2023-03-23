efa536d9c370ebc209f0574c6def4feeaf809684
Nescura <flowingosirias@gmail.com>
2023-03-23 12:57:37 +0800

    Fixed up and cleaned up Stacker mini game

--- Popup Defender/Assets/2. Scripts/Minigame Scripts/PanelTowerBuilder.cs
+++ Popup Defender/Assets/2. Scripts/Minigame Scripts/PanelTowerBuilder.cs
@@ -12,7 +12,7 @@ public class PanelTowerBuilder : IPanelStrategy
     private GameObject previousBlock;
     private int blockCount;
     private float blockOffset;
-    float direction = 5f;
+    float direction = 3f;
     private List<GameObject> blockList = new List<GameObject>();
 
     public Vector2 SetPanelSize() => new Vector2(sizeX, sizeY);
@@ -24,12 +24,12 @@ public class PanelTowerBuilder : IPanelStrategy
     {
         myPanel = panelParent;
         myDisplay = displayParent;
-        sizeX = 3f;
-        sizeY = 3f;
+        sizeX = 2f;
+        sizeY = 2.5f;
 
         if(buildingBlockStack == null)
         {
-            buildingBlockStack = GameObject.Instantiate(Resources.Load("Building Block Stack"), Vector3.zero + myDisplay.transform.position, Quaternion.identity, myDisplay.transform) as GameObject;
+            buildingBlockStack = GameObject.Instantiate(Resources.Load("Building Block Stack"), Vector3.down * 2 + myDisplay.transform.position, Quaternion.identity, myDisplay.transform) as GameObject;
         }
 
         blockCount = 0;
@@ -55,56 +55,94 @@ public class PanelTowerBuilder : IPanelStrategy
     {
         if(previousBlock == null)
         {
-            direction = 0; //stops the active block
-            previousBlock = activeBlock; //set current active block as previous block   
-            blockCount++; // increases the block count
-            blockList[blockCount].SetActive(true); //Set next block active
-            activeBlock = blockList[blockCount]; // set new block as active block
-            activeBlock.GetComponent<SpriteRenderer>().sortingOrder = previousBlock.GetComponent<SpriteRenderer>().sortingOrder;
-            direction = 5f; //Start the active block
+            FirstBlock();
         }
         else
         {
-            if ((activeBlock.transform.position.x + (activeBlock.transform.localScale.x)/2) < previousBlock.transform.position.x - (previousBlock.transform.localScale.x)/2 || (activeBlock.transform.position.x - (activeBlock.transform.localScale.x) / 2) > previousBlock.transform.position.x + (previousBlock.transform.localScale.x) / 2)//check if the blocks are touching, this is if block are not touching
+            if ((activeBlock.transform.position.x + (activeBlock.transform.lossyScale.x)/2) < previousBlock.transform.position.x - (previousBlock.transform.lossyScale.x)/2 || (activeBlock.transform.position.x - (activeBlock.transform.lossyScale.x) / 2) > previousBlock.transform.position.x + (previousBlock.transform.lossyScale.x) / 2)//check if the blocks are touching, this is if block are not touching
             {
-                myPanel.GetComponent<Panel>().ForceTimeLeft(-1, false, true);
+                MissedBlock();
             }
             else //if the blocks are touching
             {
                 if (blockCount == blockList.Count - 1) // check if all the blocks are out, there are no blocks left
                 {
-                    direction = 0; //stops the active block
-                    myPanel.GetComponent<Panel>().SetSuccess(true);
+                    LastBlock();
                 }
                 else // There are remaining blocks
                 {
                     direction = 0; //stop the active block
-                    if (activeBlock.transform.position.x < previousBlock.transform.position.x) // check the active block against the previous block to determine side of block, active block is on the left. !!left x is negative, right x is positive!!
+                    if ((activeBlock.transform.position.x + (activeBlock.transform.lossyScale.x) / 2) < (previousBlock.transform.position.x + (previousBlock.transform.lossyScale.x) / 2) && (activeBlock.transform.position.x - (activeBlock.transform.lossyScale.x) / 2) > (previousBlock.transform.position.x - (previousBlock.transform.lossyScale.x) / 2))
                     {
-                        blockOffset = ((previousBlock.transform.position.x - previousBlock.transform.localScale.x / 2) - (activeBlock.transform.position.x - activeBlock.transform.localScale.x / 2)); //calculates blockoffset by taking left edge of active block - left edge of previous block
-                        previousBlock = activeBlock; //set current active block as previous block   
-                        blockCount++; // increases the block count
-                        blockList[blockCount].SetActive(true); //Set next block active
-                        blockList[blockCount].transform.localScale = new Vector3(previousBlock.transform.localScale.x - blockOffset, 1, 1); //reduce the scale of the new block by whatever the offset was
-                        activeBlock = blockList[blockCount]; // set new block as active block
-                        activeBlock.GetComponent<SpriteRenderer>().sortingOrder = previousBlock.GetComponent<SpriteRenderer>().sortingOrder;
-                        direction = 5f; //Start the active block
+                        BlockWithin();
+                    }
+                    else if (activeBlock.transform.position.x < previousBlock.transform.position.x) // check the active block against the previous block to determine side of block, active block is on the left. !!left x is negative, right x is positive!!
+                    {
+                        BlockOverOnLeft();
                     }
                     else //active block is on the right
                     {
-                        blockOffset = ((activeBlock.transform.position.x + activeBlock.transform.localScale.x / 2) - (previousBlock.transform.position.x + previousBlock.transform.localScale.x / 2)); //calculates blockoffset by taking left edge of active block - left edge of previous block
-                        previousBlock = activeBlock; //set current active block as previous block   
-                        blockCount++; // increases the block count
-                        blockList[blockCount].SetActive(true); //Set next block active
-                        blockList[blockCount].transform.localScale = new Vector3(previousBlock.transform.localScale.x - blockOffset, 1, 1); //reduce the scale of the new block by whatever the offset was
-                        activeBlock = blockList[blockCount]; // set new block as active block
-                        activeBlock.GetComponent<SpriteRenderer>().sortingOrder = previousBlock.GetComponent<SpriteRenderer>().sortingOrder;
-                        direction = 5f; //Start the active block
+                        BlockOverOnRight();
                     }
                 }
             }
             
         }
+    }
+
+    void FirstBlock()
+    {
+        direction = 0; //stops the active block
+        previousBlock = activeBlock; //set current active block as previous block   
+        blockCount++; // increases the block count
+        blockList[blockCount].SetActive(true); //Set next block active
+        activeBlock = blockList[blockCount]; // set new block as active block
+        activeBlock.GetComponent<SpriteRenderer>().sortingOrder = previousBlock.GetComponent<SpriteRenderer>().sortingOrder;
+        direction = 3f; //Start the active block
+    }
+    void MissedBlock()
+    {
+        myPanel.GetComponent<Panel>().ForceTimeLeft(-1, false, true);
+    }
+    void LastBlock()
+    {
+        direction = 0; //stops the active block
+        myPanel.GetComponent<Panel>().SetSuccess(true);
+    }
+    void BlockOverOnLeft()
+    {
+        myPanel.GetComponent<Panel>().ForceTimeLeft(2, false, true);
+        blockOffset = ((previousBlock.transform.position.x - previousBlock.transform.lossyScale.x / 2) - (activeBlock.transform.position.x - activeBlock.transform.lossyScale.x / 2)); //calculates blockoffset by taking left edge of active block - left edge of previous block
+        previousBlock = activeBlock; //set current active block as previous block   
+        blockCount++; // increases the block count
+        blockList[blockCount].SetActive(true); //Set next block active
+        blockList[blockCount].transform.localScale = new Vector3(previousBlock.transform.lossyScale.x - blockOffset, 0.75f, 1); //reduce the scale of the new block by whatever the offset was
+        activeBlock = blockList[blockCount]; // set new block as active block
+        activeBlock.GetComponent<SpriteRenderer>().sortingOrder = previousBlock.GetComponent<SpriteRenderer>().sortingOrder;
+        direction = 3f; //Start the active block
+    }
+    void BlockOverOnRight()
+    {
+        myPanel.GetComponent<Panel>().ForceTimeLeft(2, false, true);
+        blockOffset = ((activeBlock.transform.position.x + activeBlock.transform.lossyScale.x /2) - (previousBlock.transform.position.x + previousBlock.transform.lossyScale.x /2)); //calculates blockoffset by taking rigt edge of active block - right edge of previous block
+        previousBlock = activeBlock; //set current active block as previous block   
+        blockCount++; // increases the block count
+        blockList[blockCount].SetActive(true); //Set next block active
+        blockList[blockCount].transform.localScale = new Vector3(previousBlock.transform.lossyScale.x - blockOffset, 0.75f, 1); //reduce the scale of the new block by whatever the offset was
+        activeBlock = blockList[blockCount]; // set new block as active block
+        activeBlock.GetComponent<SpriteRenderer>().sortingOrder = previousBlock.GetComponent<SpriteRenderer>().sortingOrder;
+        direction = 3f; //Start the active block
+    }
+    void BlockWithin()
+    {
+        myPanel.GetComponent<Panel>().ForceTimeLeft(2, false, true);
+        previousBlock = activeBlock; //set current active block as previous block   
+        blockCount++; // increases the block count
+        blockList[blockCount].SetActive(true); //Set next block active
+        blockList[blockCount].transform.localScale = new Vector3(previousBlock.transform.lossyScale.x, 0.75f, 1); //reduce the scale of the new block by whatever the offset was
+        activeBlock = blockList[blockCount]; // set new block as active block
+        activeBlock.GetComponent<SpriteRenderer>().sortingOrder = previousBlock.GetComponent<SpriteRenderer>().sortingOrder;
+        direction = 3f; //Start the active block
     }
 
     public void OnControlHold()
@@ -120,8 +158,8 @@ public class PanelTowerBuilder : IPanelStrategy
     }
     public void MiniUpdate()
     {
-        if (activeBlock.transform.localPosition.x >= 0.95 && direction > 0) direction *= -1;
-        if (activeBlock.transform.localPosition.x <= -0.95 && direction < 0) direction *= -1;
+        if (activeBlock.transform.localPosition.x >= 2.5 && direction > 0) direction *= -1;
+        if (activeBlock.transform.localPosition.x <= -2.5 && direction < 0) direction *= -1;
 
         activeBlock.transform.position += new Vector3(Time.deltaTime * direction, 0, 0);
     }
